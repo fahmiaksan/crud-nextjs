093e56396723508e81f81827a93235376ed427db
FhmiAksn <fahmiaksannugroho@gmail.com>
2025-03-10 17:44:41 +0700

    Add search component

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
-'use client';
+"use client";
 import NavbarComponent from "@/components/Navbar";
+import SearchComponent from "@/components/atoms/search";
 
 export default function Home() {
-
   return (
     <>
       <NavbarComponent />
-      <main className="flex min-h-screen flex-col items-center p-24">
+      <main className="flex min-h-screen flex-col items-center p-24 space-y-6">
         <h1 className="text-4xl font-bold">Hello world!</h1>
+        <SearchComponent />
       </main>
     </>
   );
