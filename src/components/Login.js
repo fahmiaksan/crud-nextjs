7bdd13571bb3f616fe140c035fce708015d41397
FhmiAksn <fahmiaksannugroho@gmail.com>
2025-03-13 23:48:32 +0700

    change button login

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,7 +56,7 @@ export default function LoginComponent() {
           <Button
             type="submit"
             disabled={pending}
-            className={`w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 `}
+            className={`w-full ${pending ? 'cursor-wait' : 'cursor-pointer'} bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 `}
           >
             {
               pending ? (
@@ -65,7 +65,7 @@ export default function LoginComponent() {
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
               ) :
-                "Register"
+                "Login"
             }
           </Button>
         </form>
