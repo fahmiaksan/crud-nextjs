972ebe6da9068ec1a4b1a591c13983dc58fcbd3b
FhmiAksn <fahmiaksannugroho@gmail.com>
2025-03-13 23:51:26 +0700

    add some validation

--- src/lib/zod.js
+++ src/lib/zod.js
@@ -1,4 +1,4 @@
-import { number, object, string } from "zod";
+import { array, number, object, string } from "zod";
 
 export const RegisterSchema = object({
   fullname: string().min(3, 'Fullname must be at least 3 characters'),
@@ -19,16 +19,35 @@ export const SignInSchema = object({
     .max(32, 'Password must be at most 32 characters'),
 });
 
-export const AddClassSchema = object({ name: string().min(3, 'Class name must be at least 3 characters').max(50, 'Class name must be at most 50 characters') });
+export const AddClassSchema = object({
+  name: string().min(3, 'Class name must be at least 3 characters').max(50, 'Class name must be at most 50 characters'),
+  teacherId: array(number()).optional(),
+});
 export const UpdateClassSchema = object({
   name: string().min(3, "Class name must be at least 3 characters").max(50, "Class name must be at most 50 characters"),
+  teacherId: array(number()).optional(),
 });
 export const AddSubjectSchema = object({
   name: string().min(2, "Subject name must be at least 2 characters").max(50, "Subject name must be at most 50 characters"),
-  teacherId: string().optional().nullable(),
+  teacherId: array(number()).optional(),
 });
 export const UpdateSubjectSchema = object({
   id: number(),
   name: string().min(2, "Subject name must be at least 2 characters").max(50, "Subject name must be at most 50 characters").optional(),
-  teacherId: string().optional().nullable(),
+  teacherId: array(number()).optional(),
+});
+
+export const AddTeacherSchema = object({
+  fullname: string().min(3, "Fullname must be at least 3 characters").max(50, "Fullname must be at most 50 characters"),
+  email: string().email("Invalid email format"),
+  password: string().min(8, "Password must be at least 8 characters").max(32, "Password must be at most 32 characters"),
+  classId: array(number()).optional(),
+  subjectId: array(number()).optional(),
+});
+
+export const updatedTeacherSchema = object({
+  fullname: string().min(2, "Subject name must be at least 2 characters").max(50, "Subject name must be at most 50 characters").optional(),
+  email: string().email("Invalid email format"),
+  subjectId: array(number()).optional(),
+  classId: array(number()).optional(),
 });
